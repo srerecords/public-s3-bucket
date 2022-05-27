36ac8e21d6aa3597c045040f6679e0e24940ad01
root <root@ip-172-31-0-179.eu-central-1.compute.internal>
2022-05-27 14:52:45 +0000

    Complete

--- aws_lambda.tf
+++ aws_lambda.tf
@@ -2,36 +2,24 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
-
 provider "aws" {
   region          = "${var.aws_region}"
 }
 
-#module lambda_python_w_deps {
-#  source           = "github.com/makzzz1986/tf-aws-lambda-python-with-dependencies"
-#  source           = "github.com/srerecords/public-s3-bucket"
-#  script_path      = "${path.module}/test.py"
-#  pip_dependencies = ["pyfiglet==0.8.post1"]
-#}
-
-
-resource aws_lambda_function hello_world {
+resource aws_lambda_function public_bucket {
   filename         =  "./script.zip"
-  function_name    = "bucket_list_4"
-  role             = "${aws_iam_role.iam_for_check_bucket_4.arn}"
-  description      = "Lambda for testing dependencies"
-#  handler          = "${module.lambda_python_w_deps.handler_file_name}.handler"
-  handler          = "test.handler"
-#  source_code_hash = module.lambda_python_w_deps.package_sha
+  function_name    = "public_bucket_6"
+  role             = "${aws_iam_role.iam_for_check_bucket_6.arn}"
+  description      = "Searching public buckets"
+  handler          = "find_public_s3_bucket.handler"
   runtime          = "python3.8"
   timeout          = 120
   publish          = true
 }
 
 
-
-resource "aws_iam_role" "iam_for_check_bucket_4" {
-  name = "iam_for_check_bucket_4"
+resource "aws_iam_role" "iam_for_check_bucket_6" {
+  name = "iam_for_check_bucket_6"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
