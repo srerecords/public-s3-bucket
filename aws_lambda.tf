b23a7144b6b1acbfbc663305da2e13bca5e6f1b5
root <root@ip-172-31-0-179.eu-central-1.compute.internal>
2022-05-27 14:39:29 +0000

    Good

--- aws_lambda.tf
+++ aws_lambda.tf
@@ -2,24 +2,26 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
+
 provider "aws" {
   region          = "${var.aws_region}"
 }
 
-module lambda_python_w_deps {
+#module lambda_python_w_deps {
 #  source           = "github.com/makzzz1986/tf-aws-lambda-python-with-dependencies"
-  source           = "github.com/srerecords/public-s3-bucket"
-  script_path      = "${path.module}/test.py"
+#  source           = "github.com/srerecords/public-s3-bucket"
+#  script_path      = "${path.module}/test.py"
 #  pip_dependencies = ["pyfiglet==0.8.post1"]
-}
+#}
 
 
 resource aws_lambda_function hello_world {
   filename         =  "./script.zip"
-  function_name    = "bucket_list_2"
-  role             = "${aws_iam_role.iam_for_check_bucket_3.arn}"
+  function_name    = "bucket_list_4"
+  role             = "${aws_iam_role.iam_for_check_bucket_4.arn}"
   description      = "Lambda for testing dependencies"
-  handler          = "${module.lambda_python_w_deps.handler_file_name}.handler"
+#  handler          = "${module.lambda_python_w_deps.handler_file_name}.handler"
+  handler          = "test.handler"
 #  source_code_hash = module.lambda_python_w_deps.package_sha
   runtime          = "python3.8"
   timeout          = 120
@@ -28,8 +30,8 @@ resource aws_lambda_function hello_world {
 
 
 
-resource "aws_iam_role" "iam_for_check_bucket_3" {
-  name = "iam_for_check_bucket_3"
+resource "aws_iam_role" "iam_for_check_bucket_4" {
+  name = "iam_for_check_bucket_4"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
